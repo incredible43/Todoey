cecdf217b8baae330c51b2cb793ae5846705637e
Mohamed Musa <seriously1q@gmail.com>
2019-05-07 05:27:35 -0400

    to-do list complete with data source and delegate methods only

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Mohamed Musa on 5/6/19.
+//  Copyright © 2019 Mohamed Musa. All rights reserved.
+//
+
+import UIKit
+class ToDoListViewController: UITableViewController {
+    let itemArray = ["scammer gets scammed"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
